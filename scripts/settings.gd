39329f9ec4c608857b36c8e57f76a7634fc17e0b
salem404 <salem4O4@proton.me>
2026-02-13 13:23:51 +0100

    feat: 🎉 Commit inicial

---
+++ scripts/settings.gd
@@ -0,0 +1,34 @@
+class_name Settings
+extends Control
+
+signal settings_closed
+
+func _ready() -> void:
+	_load_last_tab()
+
+func _load_last_tab() -> void:
+	var tab_container = get_node_or_null("MarginContainer/TabContainer")
+	if tab_container:
+		var last_tab: int = get_node("/root/ConfigManager").get_setting("settings", "last_tab", 0)
+		tab_container.current_tab = last_tab
+		tab_container.tab_changed.connect(_on_tab_changed)
+
+func _on_tab_changed(tab_index: int) -> void:
+	get_node("/root/ConfigManager").set_setting("settings", "last_tab", tab_index)
+
+func show_settings() -> void:
+	visible = true
+
+
+func _on_close_button_pressed() -> void:
+	visible = false
+	settings_closed.emit()
+	
+
+func get_all_buttons(node: Node) -> Array:
+	var buttons = []
+	for child in node.get_children():
+		if child is Button:
+			buttons.append(child)
+		buttons += get_all_buttons(child)
+	return buttons
