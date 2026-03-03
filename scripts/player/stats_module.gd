48e0535b8d3a888d092fdf7c086c3c3e3b3efbd6
EnderLz <davidsonilopez25@gmail.com>
2026-03-03 12:28:43 +0100

    feat: added base GUI

--- scripts/player/stats_module.gd
+++ scripts/player/stats_module.gd
@@ -1,6 +1,7 @@
 extends Node
 
 @export var Health: float = 100
+@export var Magic: float = 100
 @export var Speed: Vector2 = Vector2(300,200)
 @export var Jpower: float = -20
 @export var gravity: float = 1
